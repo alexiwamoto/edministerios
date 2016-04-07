d55c3ef29047e75ac413855186a4c01869b70024
alexiwamoto <alex_iwamoto@hotmail.com>
2016-04-06 22:43:18 -0300

    ajustes na cor

--- src/main/java/com/eministerios/www/business/workerUi/InitView.java
+++ src/main/java/com/eministerios/www/business/workerUi/InitView.java
@@ -27,8 +27,7 @@ public class InitView extends JFrame {
 
     public void setUpLayout(){
         setTitle("JOBS");
-        setSize(460, 160);
-        //setLocation(400,200);
+        setSize(460, 100);
         setLocationRelativeTo(null);
     }
 
@@ -38,18 +37,18 @@ public class InitView extends JFrame {
 
     public void addComponents(){
         JPanel jPanel = new JPanel();
-        jPanel.setBackground(new Color(0,0,0) /*new Color(51, 102, 153)*/);
+        jPanel.setBackground(new Color(51, 102, 153));
         jPanel.setLayout(new MigLayout("", "[grow]", "[grow][]"));
 
 
         progressBar = new JProgressBar();
 
         progressBar.setStringPainted(true);
-        progressBar.setForeground(Color.DARK_GRAY);
+        progressBar.setForeground(Color.BLACK);
         progressBar.setFont(new Font("Arial", Font.BOLD, 14));
         progressBar.setString("Loading...");
 
-        ColorUIResource colorResource = new ColorUIResource(/*Color.green.brighter()*/ Color.red.brighter().brighter());
+        ColorUIResource colorResource = new ColorUIResource(Color.blue.brighter().brighter());
         UIManager.put("nimbusOrange",colorResource);
 
         progressBar.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
@@ -75,20 +74,5 @@ public class InitView extends JFrame {
     public void setProgress(int value) {
         progressBar.setIndeterminate(false);
         progressBar.setValue(value);
-    }
-
-    class FillPainter implements Painter<JComponent> {
-
-        private final Color color;
-
-        FillPainter(Color c) {
-            color = c;
-        }
-
-        @Override
-        public void paint(Graphics2D g, JComponent object, int width, int height) {
-            g.setColor(color);
-            g.fillRect(0, 0, width - 1, height - 1);
-        }
     }
 }
